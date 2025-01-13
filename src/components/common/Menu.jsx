02e0e8cbeef2f2f081da1b9db4738fdbf53ad661
Francisco Tejerizo <frantejerizo12@gmail.com>
2025-01-13 09:16:14 -0300

    agrego clase para extender el footer al final, y realizo maquetado del navbar con barra de busqueda

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import { Button, Col, Container, Form, Nav, Navbar, Row } from "react-bootstrap";
+import { Link, NavLink } from "react-router";
 
 const Menu = () => {
     return (
-        <div>
-            
-        </div>
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Link to={'/'} className="navbar-brand">
+                    <img
+                        src=''
+                        alt="logo ecommerce gamer"
+                        className="img-fluid"
+                        width={'150px'}
+                    />
+                </Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className="d-flex justify-content-end">
+                    <Form>
+                        <Row>
+                            <Col>
+                                <Form.Control type="text" placeholder="Buscar Juego"></Form.Control>
+                            </Col>
+                            <Col>
+                                <Button type="submit" variant="success">Buscar</Button>
+                            </Col>
+                        </Row>
+                    </Form>
+
+                    <Nav>
+                        <NavLink end className='nav-link' to={'/'}>Inicio</NavLink>
+                        <NavLink end className='nav-link' to={'/acerca-de-nosotros'}>Acerca de Nosotros</NavLink>
+                        <NavLink end className='nav-link' to={'/login'}>Login</NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Inicio = () => {
     return (
-        <div>
+        <div className='mainSection'>
             
         </div>
     );
