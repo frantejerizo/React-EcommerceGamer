9b9b770173dabf37fa54d0cc20e95ecbe426b524
Tomas <pando91218@gmail.com>
2025-01-15 17:05:30 -0300

    realizamos el encabezado, dentro de este se encuentra el titulo, junto con una casilla y un boton para agregar el videojuego

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { PlusCircle } from "react-bootstrap-icons";
 
 const Administrador = () => {
     return (
-        <div>
+        <section className='mainSection mb-4'>
+            <h1 className='text-center mt-5'>Agregar videojuego</h1>
             
-        </div>
+            <Form className='container d-flex'>       
+      <Form.Group className="w-100" controlId="exampleForm.ControlInput1">
+        <Form.Control type="text" placeholder="Agregar videojuego" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+      <PlusCircle></PlusCircle>
+      </Button>
+    </Form>
+    
+        </section>
     );
 };
 
