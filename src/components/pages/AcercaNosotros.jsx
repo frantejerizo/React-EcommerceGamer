19c5d149c1c3d96b775c7954126199fc858468c2
Juan Andrés Stegmayer <stegmayerjuan63@gmail.com>
2025-01-26 23:55:54 -0300

    se creó una estrucutura base sin completar de la página AcercaNosotros

--- src/components/pages/AcercaNosotros.jsx
+++ src/components/pages/AcercaNosotros.jsx
@@ -1,7 +1,83 @@
 import React from "react";
 
 const AcercaNosotros = () => {
-  return <div></div>;
+  return (
+    <section class="container-2 rounded-4 ">
+      <h1 class="">Quienes Somos</h1>
+      <section class="d-flex flex-column  align-items-center flex-wrap m-2">
+        <article class="integrante1 row w-75 d-flex align-items-center m-2  ">
+          <div class="col-md-6 text-end"></div>
+          <div class="col-md-6 text-end">
+            <h3>Alejo Fernandez</h3>
+            <p>
+              Tengo 18 años y soy responsable de la Busqueda de Informacion de
+              la pagina, mis tareas como Desarrollador fueron: main sobre
+              nosotros, main categoria de pelicula y navbar cartelera
+            </p>
+            <p class="card-text text-light">
+              <small class="text-body-light">Tucuman-Argentina</small>
+            </p>
+          </div>
+        </article>
+        <article class="integrante2 row w-75 d-flex align-items-center m-2">
+          <div class="col-md-6 text-start">
+            <h3>Tobias Morienega</h3>
+            <p>
+              De 20 años y estoy a cargo del Contenido Audiovisual de la pagina,
+              como desarrollador trabaje sobre: el main detalle de pelicula,
+              navbar gratis y pagina inicio de sesion.
+            </p>
+            <p class="card-text text-light">
+              <small class="text-body-light">Tucuman-Argentina</small>
+            </p>
+          </div>
+          <div class="col-md-6 text-start"></div>
+        </article>
+        <article class="integrante1 row w-75 d-flex align-items-center m-2  ">
+          <div class="col-md-6 text-end"></div>
+          <div class="col-md-6 text-end">
+            <h3>Martin Calvimonte</h3>
+            <p>
+              Al dia de hoy tengo 36 años y me desempeñe tanto de Product Owner
+              como asi tambien de Scraum Master, de Desarrollador realice las
+              tareas: de main prueba gratis, carrusell y footer.
+            </p>
+            <p class="card-text text-light">
+              <small class="text-body-light">Cordoba-Argentina</small>
+            </p>
+          </div>
+        </article>
+        <article class="integrante4 row w-75 d-flex align-items-center m-2">
+          <div class="col-md-6 text-start">
+            <h3>Jose Manuel Carrasco</h3>
+            <p>
+              Con 26 años, soy el encargado del control de versiones en Git
+              GitHub y de subir la pagina a Netlify, mi trabajo de Desarrollador
+              fue: el main contacto, main detalle de pelicula y pagina index.
+            </p>
+            <p class="card-text text-light">
+              <small class="text-body-light">Madrid-España</small>
+            </p>
+          </div>
+          <div class="col-md-6 text-start"></div>
+        </article>
+        <article class="integrante1 row w-75 d-flex align-items-center m-2 ">
+          <div class="col-md-6 text-end"></div>
+          <div class="col-md-6 text-end">
+            <h3>Juan Andres Stegmayer</h3>
+            <p>
+              Soy un joven de 27 años, responsable del Diseño de la Pagina,
+              ahora como Desarrollador trabaje sobre: el main registro, main
+              cartelera y navbar registro.
+            </p>
+            <p class="card-text ">
+              <small class="text-body-light">Tucuman-Argentina</small>
+            </p>
+          </div>
+        </article>
+      </section>
+    </section>
+  );
 };
 
 export default AcercaNosotros;
