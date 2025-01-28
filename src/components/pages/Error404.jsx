ceaabc91932a862e7a71e8a72d1f37cdbe085f43
Ignacio Ramon <joseignacioramon24@gmail.com>
2025-01-28 03:13:04 -0300

    Realicé mejoras esteticas a la página error 404.

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React from "react";
 import error from "../../assets/error404.png";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
+import "./Error404.css";
 
 const Error404 = () => {
-    return (
-        <section className="mainSection text-center">
-        <img src={error} alt="error 404" />
-        <div>
-          <Link className="btn btn-success mt-3" to={'/'} >Volver al inicio</Link>
-        </div>
-      </section>
-    );
+  return (
+    <section className="error-section d-flex flex-column justify-content-center align-items-center text-center">
+      <img
+        src={error}
+        alt="Error 404 - Página no encontrada"
+        className="error-image img-fluid"
+      />
+
+      <div className="error-content mt-4">
+        <h1 className="error-title">¡Oops! Página no encontrada</h1>
+        <p className="error-text">
+          La página que estás buscando no existe o ha sido movida.
+        </p>
+        <Link to="/" className="error-button">
+          Volver al inicio
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Error404;
