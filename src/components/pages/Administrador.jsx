a20b17f9d05213d7c102b5d30979f4b0b88306a0
Ignacio Ramon <joseignacioramon24@gmail.com>
2025-01-28 03:56:24 -0300

    Se realizaron las ultimas mejoras a la parte administrativa de la plataforma, se hizo un upgrade en cuanto al diseño al CRUD

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
 import { FileEarmarkPlus } from "react-bootstrap-icons";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import { listarJuegosAPI } from "../helpers/queries";
 import JuegoAdministrador from "./juego/JuegoAdministrador";
 
@@ -19,42 +19,45 @@ const Administrador = () => {
       setListaJuegos(datos);
     } else {
       alert(
-        "Ha ocurrido un error, vuelve a intentar esta operacion en unos momentos"
+        "Ha ocurrido un error, vuelve a intentar esta operación en unos momentos"
       );
     }
   };
 
   return (
-    <section className="container mainSection">
-      <div className="d-flex justify-content-between align-items-center mt-5">
-        <h1 className="display-4">Juegos disponibles</h1>
-        <Link className="btn btn-primary" to={"/administrador/crear"}>
-          <FileEarmarkPlus></FileEarmarkPlus>
+    <section className="container mainSection py-4">
+      <div className="d-flex justify-content-between align-items-center mt-5 mb-4">
+        <h1 className="display-4 font-weight-bold text-primary">Juegos disponibles</h1>
+        <Link className="btn btn-primary btn-lg shadow-sm" to={"/administrador/crear"}>
+          <FileEarmarkPlus className="mr-2" />
+          Agregar Juego
         </Link>
       </div>
-      <hr />
-      <Table responsive striped bordered hover>
-        <thead>
-          <tr className="text-center">
-            <th>Cod</th>
-            <th>Nombre de Juego</th>
-            <th>Precio</th>
-            <th>Categoria</th>
-            <th>Imagen</th>
-            <th>Desarrollador</th>
-            <th>Opciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {listaJuegos.map((datosJuego) => (
-            <JuegoAdministrador
-              key={datosJuego.id}
-              datosJuego={datosJuego}
-              setListaJuegos={setListaJuegos}
-            ></JuegoAdministrador>
-          ))}
-        </tbody>
-      </Table>
+      <hr className="my-4" />
+      <div className="table-responsive">
+        <Table striped bordered hover className="shadow-sm">
+          <thead className="thead-dark">
+            <tr className="text-center">
+              <th>Cod</th>
+              <th>Nombre de Juego</th>
+              <th>Precio</th>
+              <th>Categoría</th>
+              <th>Imagen</th>
+              <th>Desarrollador</th>
+              <th>Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listaJuegos.map((datosJuego) => (
+              <JuegoAdministrador
+                key={datosJuego.id}
+                datosJuego={datosJuego}
+                setListaJuegos={setListaJuegos}
+              />
+            ))}
+          </tbody>
+        </Table>
+      </div>
     </section>
   );
 };
--- src/components/pages/FormularioJuegos.jsx
+++ src/components/pages/FormularioJuegos.jsx
@@ -3,8 +3,9 @@ import { Form, Button, Row, Col } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { crearJuegoAPI, editarJuegoAPI, obtenerUnSoloJuegoAPI } from "../helpers/queries";
 import { useNavigate, useParams } from "react-router";
+import "./FormularioJuegos.css";
 
-const FormularioJuegos = ({crearJuego}) => {
+const FormularioJuegos = ({ crearJuego }) => {
   const {
     register,
     handleSubmit,
@@ -13,65 +14,53 @@ const FormularioJuegos = ({crearJuego}) => {
     setValue,
   } = useForm();
 
-  const {id} = useParams()
-  const navegacion = useNavigate()
+  const { id } = useParams();
+  const navegacion = useNavigate();
 
-  useEffect(()=>
-  {
-    if(crearJuego===false)
-    {
-      cargarDatosJuego()
+  useEffect(() => {
+    if (crearJuego === false) {
+      cargarDatosJuego();
     }
-  },[])
+  }, []);
 
-  const cargarDatosJuego = async()=>{
-    const respuesta = await obtenerUnSoloJuegoAPI(id)
-    if (respuesta.status === 200)
-      {
-          const datos = await respuesta.json()
-          setValue('nombre', datos.nombre)
-          setValue('precio', datos.precio)
-          setValue('categoria', datos.categoria)
-          setValue('imagen', datos.imagen)
-          setValue('descripcion', datos.descripcion)
-          setValue('requisitosMinimos', datos.requisitosMinimos)
-          setValue('requisitosRecomendados', datos.requisitosRecomendados)
-          setValue('desarrollador', datos.desarrollador)
+  const cargarDatosJuego = async () => {
+    const respuesta = await obtenerUnSoloJuegoAPI(id);
+    if (respuesta.status === 200) {
+      const datos = await respuesta.json();
+      setValue("nombre", datos.nombre);
+      setValue("precio", datos.precio);
+      setValue("categoria", datos.categoria);
+      setValue("imagen", datos.imagen);
+      setValue("descripcion", datos.descripcion);
+      setValue("requisitosMinimos", datos.requisitosMinimos);
+      setValue("requisitosRecomendados", datos.requisitosRecomendados);
+      setValue("desarrollador", datos.desarrollador);
+    }
+  };
+
+  const onSubmit = async (juego) => {
+    if (crearJuego === true) {
+      const respuesta = await crearJuegoAPI(juego);
+      if (respuesta.status === 201) {
+        alert("El juego fue creado correctamente");
+        navegacion("/administrador");
+      } else {
+        alert("Ha ocurrido un error, vuelve a intentar esta operación en unos momentos");
       }
-  }
-
-  const onSubmit = async(juego) => {
-    if(crearJuego === true)
-    {
-      const respuesta = await crearJuegoAPI(juego)
-      if (respuesta.status === 201)
-        {
-            alert('El juego fue creado correctamente')
-            navegacion('/administrador')
-        }
-        else
-        {
-            alert('Ha ocurrido un error, vuelve a intentar esta operacion en unos momentos')
-        }
-    }
-    else
-    {
-      const respuesta = await editarJuegoAPI(juego,id)
-      if (respuesta.status === 200)
-        {
-            alert('El juego fue actualizado')
-            navegacion('/administrador')
-        }
-        else
-        {
-            alert('Ha ocurrido un error, vuelve a intentar esta operacion en unos momentos')
-        }
+    } else {
+      const respuesta = await editarJuegoAPI(juego, id);
+      if (respuesta.status === 200) {
+        alert("El juego fue actualizado");
+        navegacion("/administrador");
+      } else {
+        alert("Ha ocurrido un error, vuelve a intentar esta operación en unos momentos");
+      }
     }
   };
 
   return (
     <section className="container mainSection">
-      <h1 className="display-4 mt-5">Agregar Juego</h1>
+      <h1 className="display-4 mt-5">{crearJuego ? "Agregar Juego" : "Editar Juego"}</h1>
       <hr />
       <Form className="my-4" onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3" controlId="formNombre">
@@ -83,17 +72,15 @@ const FormularioJuegos = ({crearJuego}) => {
               required: "El nombre es un dato obligatorio",
               minLength: {
                 value: 2,
-                message: "Debe ingresar como minimo 2 caracteres",
+                message: "Debe ingresar como mínimo 2 caracteres",
               },
               maxLength: {
                 value: 70,
-                message: "Debe ingresar como maximo 70 caracteres inclusive",
+                message: "Debe ingresar como máximo 70 caracteres",
               },
             })}
           />
-          <Form.Text className="text-danger">
-            {errors.nombre?.message}
-          </Form.Text>
+          <Form.Text className="text-danger">{errors.nombre?.message}</Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formPrecio">
@@ -105,41 +92,35 @@ const FormularioJuegos = ({crearJuego}) => {
               required: "El precio es un valor obligatorio",
               min: {
                 value: 0,
-                message:
-                  "El precio minimo es de 0 pesos",
+                message: "El precio mínimo es de 0 pesos",
               },
               max: {
                 value: 100000,
-                message:
-                  "El precio maximo es de 100000 pesos",
+                message: "El precio máximo es de 100000 pesos",
               },
             })}
           />
-          <Form.Text className="text-danger">
-            {errors.precio?.message}
-          </Form.Text>
+          <Form.Text className="text-danger">{errors.precio?.message}</Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formCategoria">
           <Form.Label>Categoría*</Form.Label>
           <Form.Select
             {...register("categoria", {
-              required: "Debe seleccionar una categoria",
+              required: "Debe seleccionar una categoría",
             })}
           >
-            <option value="">Seleccione una opcion</option>
+            <option value="">Seleccione una opción</option>
             <option value="Sandbox">Sandbox</option>
-            <option value="Simulacion">Simulacion</option>
-            <option value="Fabricacion">Fabricacion</option>
+            <option value="Simulacion">Simulación</option>
+            <option value="Fabricacion">Fabricación</option>
             <option value="SoulsLike">SoulsLike</option>
-            <option value="Construccion">Construccion</option>
+            <option value="Construccion">Construcción</option>
             <option value="Aventura">Aventura</option>
             <option value="Horror">Horror</option>
             <option value="Shooter">Shooter</option>
           </Form.Select>
-          <Form.Text className="text-danger">
-            {errors.categoria?.message}
-          </Form.Text>
+          <Form.Text className="text-danger">{errors.categoria?.message}</Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formImagen">
@@ -148,116 +129,82 @@ const FormularioJuegos = ({crearJuego}) => {
             type="text"
             placeholder="Ej: https://store-images.s-microsoft.com/image/apps.61214.71827372323164480.6e97c7d2-899a-404f-8660-d622a7fc9162.740b1351-e659-486b-9d1c-948e0d928ead?q=90&w=480&h=270"
             {...register("imagen", {
-              required: "La url de la imagen es obligatoria",
+              required: "La URL de la imagen es obligatoria",
               pattern: {
                 value: /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/,
-                message:
-                  "Debe ingresar una url de imagen valida, los formatos admitidos son (jpg|jpeg|gif|png) ",
+                message: "Debe ingresar una URL de imagen válida (jpg, jpeg, gif, png)",
               },
             })}
           />
-          <Form.Text className="text-danger">
-            {errors.imagen?.message}
-          </Form.Text>
+          <Form.Text className="text-danger">{errors.imagen?.message}</Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formDescripcion">
           <Form.Label>Descripción*</Form.Label>
           <Form.Control
-            type="text"
-            placeholder="Ej: Dark Souls III es un juego de rol de acción de 2016 desarrollado por FromSoftware y publicado por Bandai Namco Entertainment para PlayStation 4, Xbox One y PC. Tercera y última entrega de la saga Dark Souls, se juega en una perspectiva en tercera persona, y los jugadores tienen acceso a varias armas, armaduras, magia y consumibles que pueden usar para luchar contra sus enemigos. Hidetaka Miyazaki, el creador de la saga, volvió para dirigir el juego tras dejar las tareas de desarrollo de Dark Souls II en manos de otros."
             as="textarea"
+            placeholder="Ej: Dark Souls III es un juego de rol de acción..."
             style={{ resize: "none", height: "100px" }}
             {...register("descripcion", {
               required: "La descripción es obligatoria",
               minLength: {
                 value: 5,
-                message:
-                  "Debe ingresar como minimo una descripción de 5 caracteres",
+                message: "Debe ingresar como mínimo 5 caracteres",
               },
               maxLength: {
                 value: 1000,
-                message:
-                  "Debe ingresar como maximo una descripción de 1000 caracteres",
+                message: "Debe ingresar como máximo 1000 caracteres",
               },
             })}
           />
-          <Form.Text className="text-danger">
-            {errors.descripcion?.message}
-          </Form.Text>
+          <Form.Text className="text-danger">{errors.descripcion?.message}</Form.Text>
         </Form.Group>
 
         <Row>
-            <Col>
-                <Form.Group className="mb-3" controlId="formRequisitosMinimos">
-                    <Form.Label>Requisitos Minimos del Sistema*</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Ej: SO *: Windows 7 SP1 64bit, Windows 8.1 64bit Windows 10 64bit
-Procesador: Intel Core i3-2100 / AMD® FX-6300
-Memoria: 4 GB de RAM
-Gráficos: NVIDIA® GeForce GTX 750 Ti / ATI Radeon HD 7950
-DirectX: Versión 11
-Red: Conexión de banda ancha a Internet
-Almacenamiento: 25 GB de espacio disponible
-Tarjeta de sonido: DirectX 11 sound device
-Notas adicionales: Internet connection required for online play and product activation"
-                        as="textarea"
-                        style={{ resize: "none", height: "150px" }}
-                        {...register("requisitosMinimos", {
-                        required: "Los requisitos Minimos del sistema son un dato obligatorio",
-                        minLength: {
-                            value: 5,
-                            message:
-                            "Debe ingresar como minimo 5 caracteres",
-                        },
-                        maxLength: {
-                            value: 1000,
-                            message:
-                            "Debe ingresar como maximo 1000 caracteres",
-                        },
-                        })}
-                    />
-                    <Form.Text className="text-danger">
-                        {errors.requisitosMinimos?.message}
-                    </Form.Text>
-                </Form.Group>
-            </Col>
-            <Col>
-                <Form.Group className="mb-3" controlId="formRequisitosRecomendados">
-                    <Form.Label>Requisitos Recomendados del Sistema*</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Ej: SO *: Windows 7 SP1 64bit, Windows 8.1 64bit Windows 10 64bit
-Procesador: Intel Core i7-3770 / AMD® FX-8350
-Memoria: 8 GB de RAM
-Gráficos: NVIDIA® GeForce GTX 970 / ATI Radeon R9 series
-DirectX: Versión 11
-Red: Conexión de banda ancha a Internet
-Almacenamiento: 25 GB de espacio disponible
-Tarjeta de sonido: DirectX 11 sound device
-Notas adicionales: Internet connection required for online play and product activation"
-                        as="textarea"
-                        style={{ resize: "none", height: "150px" }}
-                        {...register("requisitosRecomendados", {
-                        required: "Los requisitos Recomendados del sistema son un dato obligatorio",
-                        minLength: {
-                            value: 5,
-                            message:
-                            "Debe ingresar como minimo 5 caracteres",
-                        },
-                        maxLength: {
-                            value: 1000,
-                            message:
-                            "Debe ingresar como maximo 1000 caracteres",
-                        },
-                        })}
-                    />
-                    <Form.Text className="text-danger">
-                        {errors.requisitosRecomendados?.message}
-                    </Form.Text>
-                </Form.Group>
-            </Col>
+          <Col>
+            <Form.Group className="mb-3" controlId="formRequisitosMinimos">
+              <Form.Label>Requisitos Mínimos del Sistema*</Form.Label>
+              <Form.Control
+                as="textarea"
+                placeholder="Ej: SO: Windows 7 SP1 64bit..."
+                style={{ resize: "none", height: "150px" }}
+                {...register("requisitosMinimos", {
+                  required: "Los requisitos mínimos son obligatorios",
+                  minLength: {
+                    value: 5,
+                    message: "Debe ingresar como mínimo 5 caracteres",
+                  },
+                  maxLength: {
+                    value: 1000,
+                    message: "Debe ingresar como máximo 1000 caracteres",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">{errors.requisitosMinimos?.message}</Form.Text>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group className="mb-3" controlId="formRequisitosRecomendados">
+              <Form.Label>Requisitos Recomendados del Sistema*</Form.Label>
+              <Form.Control
+                as="textarea"
+                placeholder="Ej: SO: Windows 10 64bit..."
+                style={{ resize: "none", height: "150px" }}
+                {...register("requisitosRecomendados", {
+                  required: "Los requisitos recomendados son obligatorios",
+                  minLength: {
+                    value: 5,
+                    message: "Debe ingresar como mínimo 5 caracteres",
+                  },
+                  maxLength: {
+                    value: 1000,
+                    message: "Debe ingresar como máximo 1000 caracteres",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">{errors.requisitosRecomendados?.message}</Form.Text>
+            </Form.Group>
+          </Col>
         </Row>
 
         <Form.Group className="mb-3" controlId="formDesarrollador">
@@ -269,20 +216,18 @@ Notas adicionales: Internet connection required for online play and product acti
               required: "El desarrollador es un dato obligatorio",
               minLength: {
                 value: 2,
-                message: "Debe ingresar como minimo 2 caracteres",
+                message: "Debe ingresar como mínimo 2 caracteres",
               },
               maxLength: {
                 value: 70,
-                message: "Debe ingresar como maximo 70 caracteres inclusive",
+                message: "Debe ingresar como máximo 70 caracteres",
               },
             })}
           />
-          <Form.Text className="text-danger">
-            {errors.desarrollador?.message}
-          </Form.Text>
+          <Form.Text className="text-danger">{errors.desarrollador?.message}</Form.Text>
         </Form.Group>
 
-        <Button type="submit" variant="success">
+        <Button type="submit" variant="success" className="btn-guardar">
           Guardar
         </Button>
       </Form>
